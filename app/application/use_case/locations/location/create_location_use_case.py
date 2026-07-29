a5d6b87a69c6a4e3b0053c96e5c1cbccea21c90c
Rajib Mondal <rajib.3t@gmail.com>
2026-07-29 14:08:26 +0530

    add location api

--- app/application/use_case/locations/location/create_location_use_case.py
+++ app/application/use_case/locations/location/create_location_use_case.py
@@ -1,4 +1,5 @@
 from app.application.dto.locations.location import LocationDTO
+from app.core.exceptions import AppException
 from app.application.use_case.base_use_case import BaseUseCase
 from app.models.location_model import Location
 from app.services.location_service import LocationService
@@ -16,7 +17,46 @@ class CreateLocationUseCase(BaseUseCase):
         self.city_service = city_service
         self.current_user = current_user
     
-    def execute(self, location_data: LocationDTO) -> Location:
-        
+    async def execute(self, location_data: LocationDTO) -> Location:
+        city = await self.city_service.get_by_public_id(
+            public_id=location_data.city_id,
+            with_relations={"country": True},
+            flush=True,
+        )
+        if not city:
+            raise AppException(
+                status_code=404,
+                message="City not found",
+                error_code="CITY_NOT_FOUND",
+                field="city_id",
+            )
 
-        return self.service.create_location(location_data)
+        existing_location = await self.service.location_repository.get_by_name_and_city_id(
+            name=location_data.name,
+            city_id=city.id,
+            flush=True,
+        )
+        if existing_location:
+            raise AppException(
+                status_code=409,
+                message="Location name already exists in this city",
+                error_code="LOCATION_NAME_EXIST",
+                field="name",
+            )
+
+        location = Location(
+            name=location_data.name,
+            city_id=city.id,
+            created_by=self.current_user.id,
+            updated_by=self.current_user.id,
+        )
+        created_location = await self.service.create(
+            location,
+            with_relations=None,
+            commit=True,
+        )
+
+        return await self.service.location_repository.get_by_id_with_city_country(
+            created_location.id,
+            flush=True,
+        ) or created_location
--- app/repositories/location_repository.py
+++ app/repositories/location_repository.py
@@ -1,5 +1,9 @@
 from typing import Optional, TypedDict
 
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
+from app.models.city_model import City
 from app.models.location_model import Location
 from app.repositories.base_repository import BaseRepository
 
@@ -30,3 +34,34 @@ class LocationRepository(BaseRepository[Location]):
             await self.db.commit()
             await self.db.refresh(location)
         return location
+
+    async def get_by_name_and_city_id(
+        self,
+        name: str,
+        city_id: int,
+        with_relations: Optional[WithRelations] = None,
+        flush: bool = False,
+    ) -> Optional[Location]:
+        query = self._apply_relations(
+            select(Location).where(
+                Location.city_id == city_id,
+                Location.name.ilike(name.strip()),
+            ),
+            with_relations,
+            self._relation_map,
+        )
+        return await self._fetch_one(query, flush=flush)
+
+    async def get_by_id_with_city_country(
+        self,
+        location_id: int,
+        flush: bool = False,
+    ) -> Optional[Location]:
+        query = (
+            select(Location)
+            .where(Location.id == location_id)
+            .options(
+                selectinload(Location.city).selectinload(City.country)
+            )
+        )
+        return await self._fetch_one(query, flush=flush)
